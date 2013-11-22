16fada2ea6f7d496a20ad5fba57d49a7a7e92d3d
Codeliner <kontakt@codeliner.ws>
2013-11-22 22:10:17 +0100

    Add PHPUnit structure

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -32,6 +32,8 @@ class Module
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    'Domain' => __DIR__ . '/src/Domain',
+                    'Infrastructure' => __DIR__ . 'src/Infrastructure'
                 ),
             ),
         );
