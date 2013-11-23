af8e5f181d8558c6454bfe48c0a49de1cb3f6551
Codeliner <kontakt@codeliner.ws>
2013-11-23 15:19:13 +0100

    Add doctrine support to base TestCase

--- module/Application/tests/PHPUnit/ApplicationTest/TestCase.php
+++ module/Application/tests/PHPUnit/ApplicationTest/TestCase.php
@@ -9,6 +9,7 @@
 namespace ApplicationTest;
 
 use PHPUnit_Framework_TestCase;
+use Doctrine\ORM\EntityManager;
 /**
  *  TestCase
  * 
@@ -16,5 +17,68 @@ use PHPUnit_Framework_TestCase;
  */
 class TestCase extends PHPUnit_Framework_TestCase
 {
+    protected $entityManager;
+    protected $schemaTool;
     
+    /**
+     *
+     * @return EntityManager
+     */
+    public function getTestEntityManager()
+    {
+        if (null === $this->entityManager) {
+            $conn = \Doctrine\DBAL\DriverManager::getConnection(array(
+                'driver' => 'pdo_sqlite',
+                'memory' => true
+            ));
+
+            $config = new \Doctrine\ORM\Configuration();
+
+            $config->setAutoGenerateProxyClasses(true);
+            $config->setProxyDir(\sys_get_temp_dir());
+            $config->setProxyNamespace(get_class($this) . '\Entities');
+            $config->setMetadataDriverImpl(
+                new \Doctrine\ORM\Mapping\Driver\AnnotationDriver(
+                    new \Doctrine\Common\Annotations\IndexedReader(
+                        new \Doctrine\Common\Annotations\AnnotationReader()
+                    )
+                )
+            );
+
+            $config->setQueryCacheImpl(new \Doctrine\Common\Cache\ArrayCache());
+            $config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache());
+
+            $this->entityManager = \Doctrine\ORM\EntityManager::create(array(
+                'driver' => 'pdo_sqlite',
+                'memory' => true
+            ), $config);
+        }
+
+        return $this->entityManager;
+    }
+
+    public function createEntitySchema($entityNameOrNamespace, $pathToEntityDir = null)
+    {
+        if (!is_null($pathToEntityDir)) {
+            $dir = opendir($pathToEntityDir);
+
+            $entityNameOrNamespace = trim($entityNameOrNamespace, '\\');
+
+            while($file = readdir($dir)) {
+                if (0 !== strpos($file, '.')) {
+                    $entityClass = $entityNameOrNamespace . '\\' . str_replace('.php', '', $file);
+                    $this->createEntitySchema($entityClass);
+                }
+            }
+
+            return;
+        }
+
+        if (null === $this->schemaTool) {
+            $this->schemaTool = new \Doctrine\ORM\Tools\SchemaTool($this->getTestEntityManager());
+        }
+        $schema = $this->getTestEntityManager()->getClassMetadata($entityNameOrNamespace);
+        $this->schemaTool->dropSchema(array($schema));
+        $this->schemaTool->createSchema(array($schema));
+    }
 }
