3686f0783fd7d71df18b7a8c8eca6ca7ebaabe7c
Codeliner <kontakt@codeliner.ws>
2014-01-10 23:54:44 +0100

    Add Itinerary implementation

---
+++ module/Application/tests/PHPUnit/ApplicationTest/Domain/Model/Cargo/ItineraryTest.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of the codeliner/php-ddd-cargo-sample package.
+ * (c) Alexander Miertsch <kontakt@codeliner.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ApplicationTest\Domain\Model\Cargo;
+
+use Application\Domain\Model\Cargo\Itinerary;
+use ApplicationTest\TestCase;
+use Doctrine\Common\Collections\ArrayCollection;
+use Application\Domain\Model\Cargo\Leg;
+/**
+ * Class ItineraryTest
+ * 
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class ItineraryTest extends TestCase
+{
+    public function testLegs()
+    {
+        $legs = new ArrayCollection();
+        
+        $legs->add(new Leg('Hongkong', 'Hamburg'));
+        $legs->add(new Leg('Hamburg', 'New York'));
+        
+        $itinerary = new Itinerary($legs);
+        
+        $this->assertSame($legs, $itinerary->legs());
+    }
+    
+    public function testSameValueAs()
+    {
+        $legs = new ArrayCollection();
+        
+        $legs->add(new Leg('Hongkong', 'Hamburg'));
+        $legs->add(new Leg('Hamburg', 'New York'));
+        
+        $itinerary = new Itinerary($legs);
+        $sameItinerary = new Itinerary($legs);
+        
+        $otherLegs = new ArrayCollection();
+        $otherLegs->add( new Leg('New York', 'Melbourne'));
+        $otherLegs->add( new Leg('Melbourne', 'Rotterdam'));
+        
+        $otherItinerary = new Itinerary($otherLegs);
+        
+        $this->assertTrue($itinerary->sameValueAs($sameItinerary));
+        $this->assertFalse($itinerary->sameValueAs($otherItinerary));
+    }
+}
