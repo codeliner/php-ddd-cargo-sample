0822960561407ed47db2cfbcc117b6f0bdeb461a
Witold Wasiczko <witold@wasiczko.pl>
2017-10-07 22:24:15 +0200

    Fix container factory

--- config/container.php
+++ config/container.php
@@ -2,11 +2,11 @@
 
 use Zend\ServiceManager\ServiceManager;
 
-return function(): ServiceManager {
+return (function(): ServiceManager {
     $config = require __DIR__ .'/config.php';
     $dependencies = $config['dependencies'];
     $dependencies['services']['config'] = $config;
 
     // Build container
     return new ServiceManager($dependencies);
-};
+})();
